c1a37ee624ec1f4966b57b7d92aa5aea38f3ac2b
DvTonder <david.vantonder@gmail.com>
2012-12-08 19:58:31 -0500

    Messaging:  Only allow Mark as read when Quick reply is active
    Change-Id: I1d3e803f28d8f7bd52059deb5a48f861ddf01545

--- src/com/android/mms/transaction/MessagingNotification.java
+++ src/com/android/mms/transaction/MessagingNotification.java
@@ -1024,6 +1024,16 @@ public class MessagingNotification {
                 PendingIntent qmPendingIntent = PendingIntent.getActivity(context, 0, qmIntent,
                         PendingIntent.FLAG_UPDATE_CURRENT);
                 noti.addAction(R.drawable.ic_reply, qmText, qmPendingIntent);
+
+                // Add the 'Mark as read' action
+                CharSequence markReadText = context.getText(R.string.qm_mark_read);
+                Intent mrIntent = new Intent();
+                mrIntent.setClass(context, QmMarkRead.class);
+                mrIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
+                mrIntent.putExtra(QmMarkRead.SMS_THREAD_ID, mostRecentNotification.mThreadId);
+                PendingIntent mrPendingIntent = PendingIntent.getActivity(context, 0, mrIntent,
+                        PendingIntent.FLAG_UPDATE_CURRENT);
+                noti.addAction(R.drawable.ic_menu_done_holo_dark, markReadText, mrPendingIntent);
             }
 
             // Add the Call action
@@ -1032,7 +1042,7 @@ public class MessagingNotification {
             callIntent.setData(mostRecentNotification.mSender.getPhoneUri());
             PendingIntent mCallPendingIntent = PendingIntent.getActivity(context, 0, callIntent,
                     PendingIntent.FLAG_UPDATE_CURRENT);
-            noti.addAction(R.drawable.ic_menu_call, callText, mCallPendingIntent);
+            noti.addAction(R.drawable.ic_menu_call, callText, callPendingIntent);
         }
 
         if (messageCount == 1) {
